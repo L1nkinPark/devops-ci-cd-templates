c6a0ef6135fceb2f0560da4b80d3853d4ef98284
L1nkinPark <hieuvdfs91539@fpt.edu.vn>
2026-06-21 03:08:09 +0700

    feat(aws): complete Lab 5 - Integrate API Gateway and Cognito and add screenshots

--- cloud/aws/lab/9. API Gateway & Cognito/4. Lab 4 - Cognito Hosted UI Login and Token/exchange-token.py
+++ cloud/aws/lab/9. API Gateway & Cognito/4. Lab 4 - Cognito Hosted UI Login and Token/exchange-token.py
@@ -5,7 +5,7 @@ client_id = '23k9b82pf42m1nv9gslab0b70h'
 client_secret = '1t6g4lial29aloo5i4g5hsqb8puskogo6k2kb629nf9ddpebfruv'
 redirect_uri = 'https://h1eudayne.dev'
 token_endpoint = 'https://us-east-1tpt9lktih.auth.us-east-1.amazoncognito.com/oauth2/token'
-authorization_code = '96e9936a-0a48-44da-80b7-0da06df76949'
+authorization_code = '2be0fd25-f22e-471b-846c-0ebcc2303b4b'
 
 # Encode the client ID and client secret
 client_credentials = f"{client_id}:{client_secret}"
@@ -41,12 +41,12 @@ if response.status_code == 200:
     with open('refresh_token.txt', 'w', encoding='utf-8') as f:
         f.write(refresh_token or '')
         
-    print('=== ĐỔI TOKEN THÀNH CÔNG ===')
-    print('1. ID Token (Lưu tại id_token.txt):')
+    print('=== EXCHANGE TOKEN SUCCESS ===')
+    print('1. ID Token (Saved at id_token.txt):')
     print(f'   {id_token[:20]}...{id_token[-20:] if id_token else ""}')
-    print('2. Access Token (Lưu tại access_token.txt):')
+    print('2. Access Token (Saved at access_token.txt):')
     print(f'   {access_token[:20]}...{access_token[-20:] if access_token else ""}')
-    print('3. Refresh Token (Lưu tại refresh_token.txt):')
+    print('3. Refresh Token (Saved at refresh_token.txt):')
     print(f'   {refresh_token[:20]}...{refresh_token[-20:] if refresh_token else ""}')
 else:
     print('Failed to exchange authorization code for tokens:', response.text)
