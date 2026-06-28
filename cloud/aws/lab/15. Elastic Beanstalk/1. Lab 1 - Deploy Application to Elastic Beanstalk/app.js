427464cfcdce490e45487ed7024606501a0bc0f9
L1nkinPark <hieuvdfs91539@fpt.edu.vn>
2026-06-28 22:36:34 +0700

    Update AWS docs for ECS, ECR and Elastic Beanstalk

--- cloud/aws/lab/15. Elastic Beanstalk/1. Lab 1 - Deploy Application to Elastic Beanstalk/app.js
+++ cloud/aws/lab/15. Elastic Beanstalk/1. Lab 1 - Deploy Application to Elastic Beanstalk/app.js
@@ -2,17 +2,17 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') { 
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><h1 style="color:red;">Welcome to Website v0.2</h1></body></html>');
+        res.write('<html><body><h1 style="color:green;">Welcome to Website v0.1</h1></body></html>');
         res.end();
     }
     else if (req.url == "/training") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><h1 style="color:blue;">Welcome to Training page v0.2!</h1></body></html>');
+        res.write('<html><body><h1 style="color:blue;">Welcome to Training page v0.1!</h1></body></html>');
         res.end();
     }
     else if (req.url == "/admin") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><h1 style="color:black;">You need admin permission to access this page! - v0.2</h1></body></html>');
+        res.write('<html><body><h1 style="color:black;">You need admin permission to access this page! - v0.1</h1></body></html>');
         res.end();
     }
     else
